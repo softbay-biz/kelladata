258ad3666e25681f29541cf7263a94d1740b7996
Christian Kamdem <christiankamdem2@gmail.com>
2018-11-23 12:06:28 +0000

    Second chart type added

--- js/modules/analyse.js
+++ js/modules/analyse.js
@@ -1,20 +1,35 @@
-import survey from './../modules/form_modele.js';
+import survey from './../helpers/form_modele.js';
+import {graph} from './../modules/graph.js';
 
 document.addEventListener("DOMContentLoaded",()=>{
-  document.getElementById("execute").addEventListener("click",()=>{
+  return document.getElementById("execute").addEventListener("click",()=>{
     let variable_list = document.getElementById("variable_list");
     let section_list = document.getElementById("section_list");
     let section = section_list.options[section_list.selectedIndex].id;
-    addVariablesToList(section);
+    requestAnimationFrame(()=>{
+        //document.getElementById("myChart").innerHTML = "";
+        addVariablesToList(section);
+    })
   });
 },false);
 function addVariablesToList(section){
+ let ctx = document.getElementById("myChart").getContext('2d');
   if(section === "section_a"){
-    console.log(Object.keys(survey.section_a));
+    let tabElt = ["Arrondissement","Village"];
+    let tabVal = [5,6];
+    return graph(ctx,tabElt,tabVal,"polarArea");
   }else if(section === "section_b"){
-    console.log(Object.keys(survey.section_b));
+    let tabElt = ["Homme(s)","Femme(s)","Père","Mère","Fils/Fille","Neveu/Nice","Petits fils/fille","Conjointe",
+  "Célibataire","Marié mono","Marié poly","Veuf/Veuve","Divorcé","Séparé","Union libre"];
+  let tabVal = [5,6,62,14,56,2,3,9,7,69,36,21,45,23,25];
+    return graph(ctx,tabElt,tabVal,"radar");
   }else if(section === "section_c"){
-    console.log(Object.keys(survey.section_c));
+    let tabElt = ["Parle Bassa'a","Parle Français","Parle Anglais","Autre langue","Primaire","Secondaire","Université","Formation",
+  "NSP","Ont fréquentés","Fréquentent actuellement","Formation professionnelle/technique au secondaire","Formation générale",
+  "Formation pour enseignant : ENIET, ENIEG","Université/ Formation professionnelle universitaire","Manque de performance scolaire",
+"Etat de santé"];
+  let tabVal = [5,6,62,14,56,2,3,9,7,69,36,21,45,23,25,0,5,3];
+    return graph(ctx,tabElt,tabVal,"radar");
   }
   else if(section === "section_d"){
     console.log(Object.keys(survey.section_d));

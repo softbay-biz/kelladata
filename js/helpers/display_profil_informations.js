473286b5f513271a0922b5c7bb6f969a1948c355
Christian Kamdem <christiankamdem2@gmail.com>
2019-04-09 22:08:30 +0100

    Now can can  change profil informations / except image

--- js/helpers/display_profil_informations.js
+++ js/helpers/display_profil_informations.js
@@ -1,7 +1,17 @@
 import {uploadFiles} from './uploadFiles.js';
+import {sendData} from './sendData.js';
+
+const updateLocalInfos = ()=>{
+  localStorage.setItem("last_name",profile_informations_name.value);
+  localStorage.setItem("first_name",profile_informations_surname.value);
+  localStorage.setItem("email",informations_email.value);
+};
 const display_profil_informations = ()=>{
   const profile_display_image = document.getElementById("profile_display_image");
   const profile_pop_up_cancel = document.getElementById("profile_pop_up_cancel");
+  const profile_informations_name = document.getElementById("profile_informations_name");
+  const profile_informations_surname = document.getElementById("profile_informations_surname");
+  const profile_informations_email = document.getElementById("profile_informations_email");
   //We make the profile photo clickable
   const source_cover = document.createElement("input");
   source_cover.type = "file";
@@ -13,13 +23,33 @@ const display_profil_informations = ()=>{
     uploadFiles(source_cover.files[0],profile_display_image);
   });
   profile_pop_up_update.addEventListener("click",()=>{
-    console.log("Hello");
+    let form = {
+      requestName:btoa(btoa(btoa("updateDetails"))),
+      data:{
+        "1":["first_name",profile_informations_surname.value],
+        "2":["last_name",profile_informations_name.value],
+        email:profile_informations_email.value,
+        id:localStorage.getItem("id")
+      }
+    };
+    let updateDetails_promise = sendData(form);
+     updateDetails_promise.then((response)=>{
+       if(response.error == false){
+         updateLocalInfos();
+         alert("Profile mis à jour avec succès!");
+       }else{
+         alert("Probleme lors de la mise à jour du profil!");
+         //Here we also update profile details
+       }
+     }).catch((error)=>{
+       //console.log(error);
+     });
   },false);
   if(localStorage.getItem("photo")){
     profile_display_image.src = "medias/images/original_image/"+localStorage.getItem("photo");
-    document.getElementById("profile_informations_name").value = localStorage.getItem("last_name");
-    document.getElementById("profile_informations_surname").value = localStorage.getItem("first_name");
-    document.getElementById("profile_informations_email").value = localStorage.getItem("email");
+    profile_informations_name.value = localStorage.getItem("last_name");
+    profile_informations_surname.value = localStorage.getItem("first_name");
+    profile_informations_email.value = localStorage.getItem("email");
   }
 };
 
